212bfa828dd2c6f3da82f173ce13cecb8739bf63
adelelfar256 <30664966+adelelfar256@users.noreply.github.com>
2018-09-28 03:21:09 +0200

    Add files via upload

---
+++ LeftClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LeftClick : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+
+public GameObject go;
+	// Use this for initialization
+	 bool pressed = false;
+	bool r=false,l=false;
+movement m;
+	void Start () {
+		m=FindObjectOfType<movement>();
+	}
+   
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(r);
+if(r==true)
+go.transform.Translate(-0.01f,0.0f,0.0f);
+
+	}
+
+		 public void OnPointerDown(PointerEventData eventData)
+    {
+      r=true;
+    }
+ 
+    public void OnPointerUp(PointerEventData eventData)
+    {
+		
+r=false;
+    }
+}
---
+++ inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inventory : MonoBehaviour {
+public GameObject MyInventory;
+    public Button inv;
+
+	// Use this for initialization
+	void Start () {
+	Button btn1 = inv.GetComponent<Button>();
+    btn1.onClick.AddListener(TaskOnClick);
+
+	}
+	 void TaskOnClick()
+    {
+		MyInventory.SetActive(true);
+		Debug.Log("hey");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	  
+
+	}
+	public void ShowInv()
+	{
+		
+	}
+	}
