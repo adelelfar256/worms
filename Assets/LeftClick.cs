e3a99a341be09232215f02f8d465f0bcc069a952
adelelfar256 <adelessam256@gmail.com>
2018-09-28 15:16:00 +0200

    movement,screen

---
+++ Assets/LeftClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LeftClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject go;
+    // Use this for initialization
+    bool pressed = false;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed == true)
+        {
+            if (go.transform.position.x > -2.35f)
+                go.transform.Translate(-0.01f, 0.0f, 0.0f);
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        pressed = false;
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+ 
+
+  
+	void Start () {
+
+	}
+	
+	public void MoveRight()
+	{
+	
+
+	}
+	void MoveLeft()
+	{
+		
+	}
+	
+	// Update is( called once per frame
+	public void Update () {
+
+
+
+		
+	}
+}
